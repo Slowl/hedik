b651a262bb2366ec9b8ea0e87ae12c38e2162ebf
slowl <hedi0475@gmail.com>
2023-11-05 21:18:09 +0100

    fix: handle failing api and temporarily remove github url to avoid rate limits

--- src/data/github/index.ts
+++ src/data/github/index.ts
@@ -1,1 +1,2 @@
-export const githubReposUrl = 'https://api.github.com/users/slowl/repos';
+// export const githubReposUrl = 'https://api.github.com/users/slowl/repos';
+export const githubReposUrl = 'https://www.google.com/';
